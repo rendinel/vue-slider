042523df47138bf41e6bc438ac5626df1ff0345d
lucio rendine <luciorendine69@gmail.com>
2021-01-27 15:48:45 +0100

    push

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+ el: '#root',
+ data: {
+   images:[
+     'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg'
+   ],
+
+ },
+ methods: {
+
+  }
+});
+Vue.config.devtools = true;
